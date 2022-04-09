86790edae00536dea02606af2cf28efca1bb1271
Dmitrii <you@example.com>
2022-04-09 14:20:31 +0700

    site desc finished

--- js/main.js
+++ js/main.js
@@ -1,3 +1,9 @@
 $(function () {
-   
+   $('.design__slider').slick({
+      dots: false,
+      slideToShow: 4,
+      variableWidth: true,
+      prevArrow: '<img class="arrow arrow-left" src="images/Arrow-left.svg">',
+      nextArrow: '<img class="arrow arrow-right" src="images/Arrow-right.svg">',
+   });
 });
