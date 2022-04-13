ab5cf340b09f535f77eb5982a885e63828c38916
Dmitrii <you@example.com>
2022-04-13 16:52:41 +0700

    adaptive

--- js/main.js
+++ js/main.js
@@ -5,5 +5,15 @@ $(function () {
       variableWidth: true,
       prevArrow: '<img class="arrow arrow-left" src="images/Arrow-left.svg">',
       nextArrow: '<img class="arrow arrow-right" src="images/Arrow-right.svg">',
+      responsive: [
+         {
+            breackpoint: 361,
+            settings: {
+               variableWidth: false,
+               slideToShow: 1,
+               arrows: false,
+            }
+         }
+      ]
    });
 });
